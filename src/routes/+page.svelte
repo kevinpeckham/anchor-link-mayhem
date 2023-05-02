e1468cd0efe9167e5c225c7f2a716a1317363ad6
kevinpeckham <3825256+kevinpeckham@users.noreply.github.com>
2023-05-02 19:55:00 -0400

    quick test

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,65 +1,19 @@
 <!-- Example Svelte Page / Starter Web Page-->
 <script lang="ts">
-	// components
-	import ButtonLink from "$atoms/ButtonLink.svelte";
-	import Footer from "$atoms/Footer.svelte";
-	import LogosGrid from "$molecules/LogosGrid.svelte";
-	import PreFooter from "$atoms/PreFooter.svelte";
-
-	// store
-	import { brands } from "$stores/brandsStore";
-
-	// logos to display
-	$brands = [
-		{ name: "Svelte", url: "https://svelte.dev/" },
-		{ name: "Typescript", url: "https://www.typescriptlang.org/" },
-		{ name: "TailwindCSS", url: "https://tailwindcss.com/" },
-		{ name: "Pug", url: "https://pugjs.org/api/getting-started.html" },
-	];
 </script>
 
 <template lang="pug">
-	//- head
-	svelte:head
-		title Svelte Pug Tailwind Typescript | SkinnyPug
-		meta(
-			content="A node starter template for the Svelte Preprocessed stack.",
-			name="description"
-		)
-
 	//- body
-	main.relative.grid.min-h-screen.place-content-center.bg-primary.p-4.pb-48.text-white
-		div(class="sm:max-w-lg lg:max-w-xl xl:max-w-2xl")
-			LogosGrid
+	main.relative.grid.place-content-center.bg-primary.text-white
+		a.fixed.right-16.top-16.underline.underline-offset-4(href!="/other-page") Other Page
+		+each('["bg-red-500","bg-purple-500","bg-blue-500"] as item, index')
+			+const('sectionNumber = index + 1')
+			+const('location = (index == 0) ? "Bottom ↓" : "Top ↑"')
+			+const('targetSectionNumber = (index == 0) ? 3 : 1')
+			section.w-screen.h-screen.bg-white.grid.grid-cols-1.place-items-center(class!="{item}" id!="section-{sectionNumber}")
+				.text-24() Home Page - Section {sectionNumber}
+				+if('index != 1')
+					a(href="./#section-{targetSectionNumber}").inline-block.border.px-3.py-2.rounded.bg-black.bg-opacity-10 Scroll to {location}
 
-			.text-center(class="sm:text-left")
-				//- headline
-				h1.prose-xl.mb-3.font-semibold.text-accent
-					| Hit the ground running with Sveltekit, Typescript, TailwindCSS &amp; Pug.
 
-				//- text
-				p.text-md.prose-base.mb-10.opacity-90(class="sm:mb-8")
-					| This project is an open source starter template for Node projects using SvelteKit, Typescript, TailwindCSS, and Pug. Designed to help get you up and running quickly with the SkinnyPug Stack.
-
-				//- cta
-				ButtonLink(
-					targetUrl="https://github.com/lightning-jar/lj-sveltekit-ts-tailwind-pug-starter",
-					title="View project on Github"
-				)
-					| Clone from Github
-
-	//- pre-footer
-	PreFooter(
-		targetUrl="https://pugify.dev",
-		title="convert HTML to pug"
-	)
-		svelte:fragment(slot="default") Also check out the ad-free HTML to Pug converter @ https://pugify.dev
-
-	//- footer
-	Footer(
-		authorLink="https://fosstodon.org/@kevinpeckham",
-		orgLink="https://lightningjar.com"
-	)
-		svelte:fragment(slot="author") Kevin Peckham
-		svelte:fragment(slot="org") Lightning Jar
 </template>
---
+++ src/routes/other-page/+page.svelte
@@ -0,0 +1,12 @@
+<!--
+@component
+Test action of cross-page anchor links.
+-->
+
+<template lang='pug'>
+.bg-primary.text-white.w-screen.h-screen.px-16.py-16
+  .text-24.mb-2 The Other Page
+  .italic.mb-6 Test behavior of cross-page anchor links.
+  +each('[1,2,3] as target')
+    div: a.inline-block.border.px-3.py-2.rounded.mb-4(href="/#section-{target}") Home Page Section - {target}
+</template>
